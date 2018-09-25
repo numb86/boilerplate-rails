0d0ed98ea898144fcfec94d5ce6d3a60f6e207a5
numb86 <86numb@gmail.com>
2018-09-25 14:04:27 +0900

    プロジェクト名をユーザーが指定するようにした。

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
+I referred to [this page](https://docs.docker.com/compose/rails/).
+
 Require Docker and Docker Compose.
+And, you should update bash to over v4.
 
 1. clone
 2. bash init.sh
 3. docker-compose up
 
-I referred to [this page](https://docs.docker.com/compose/rails/).
+When you enter project name as `foo`, `YOUR_CONTAINER_NAME` is `foo_container`.
+Similarly `YOUR_DB_NAME` is `foo_db`.
+
+For example case you start `rails console`,
+
+```
+$ docker-compose exec foo_container bundle exec rails c
+```
 
 ### Rebuild the application
 
 If you make changes to the Gemfile or the Compose file to try out some different
 configurations, you need to rebuild. Some changes require only
 `docker-compose up --build`, but a full rebuild requires a re-run of
-`docker-compose run web bundle install` to sync changes in the `Gemfile.lock` to
+`docker-compose run YOUR_CONTAINER_NAME bundle install` to sync changes in the `Gemfile.lock` to
 the host, followed by `docker-compose up --build`.
--- init.sh
+++ init.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
-docker-compose run web rails new . --skip-turbolinks --force --database=postgresql
+
+read -p 'Please enter your project name: ' project_name
+while [ "$project_name" = "" ]
+do
+  read -p 'Please enter your project name: ' project_name
+done
+
+container_name="${project_name}_container"
+db_name="${project_name}_db"
+
+cat <<__END_OF_MESSAGE__ > Dockerfile
+FROM ruby:2.5
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN mkdir /${project_name}
+WORKDIR /${project_name}
+COPY Gemfile /${project_name}/Gemfile
+COPY Gemfile.lock /${project_name}/Gemfile.lock
+RUN bundle install
+COPY . /${project_name}
+
+__END_OF_MESSAGE__
+
+cat <<__END_OF_MESSAGE__ > docker-compose.yml
+version: '3'
+services:
+  ${db_name}:
+    image: postgres
+    volumes:
+      - ./tmp/db:/var/lib/postgresql/data
+  ${container_name}:
+    build: .
+    command: bundle exec rails s -p 3000 -b '0.0.0.0'
+    volumes:
+      - .:/${project_name}
+    ports:
+      - "3000:3000"
+    depends_on:
+      - ${db_name}
+
+__END_OF_MESSAGE__
+
+docker-compose run ${container_name} rails new . --skip-turbolinks --force --database=postgresql
 docker-compose build
+
 cat <<__END_OF_MESSAGE__ > config/database.yml
 default: &default
   adapter: postgresql
   encoding: unicode
-  host: db
+  host: ${db_name}
   username: postgres
   password:
   pool: 5
 
 development:
   <<: *default
-  database: myapp_development
+  database: ${project_name}_development
 
 
 test:
   <<: *default
-  database: myapp_test
+  database: ${project_name}_test
 
 __END_OF_MESSAGE__
-docker-compose run web rake db:create
+
+docker-compose run ${container_name} rake db:create
+
 cat <<__END_OF_MESSAGE__ > config/application.rb
 require_relative 'boot'
 
@@ -30,7 +74,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Myapp
+module ${project_name^}
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
